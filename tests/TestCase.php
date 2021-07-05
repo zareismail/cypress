6d02236734a8a5923d089fceff182cdf1630d21c
اسماعیل زارع <zarehesmaiel@gmail.com>
2021-07-05 18:41:55 +0430

    Uses Orchestra as base testcase

---
+++ tests/TestCase.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests;
+
+use Orchestra\Testbench\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            'Zareismail\Cypress\CoreServiceProvider',
+        ];
+    } 
+}
